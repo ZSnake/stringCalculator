d8829ebafea4307870fbb2a2c685dba6a67ab951
Viktor Zavala <viktor@acklenavenue.com>
2014-10-24 08:22:19 -0600

    first spec, installed mspec should

---
+++ stringCalculator.specs/when_adding_2_and_4.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Machine.Specifications;
+
+namespace stringCalculator.specs
+{
+    public class when_adding_2_and_4
+    {
+        Establish context =
+            () =>
+                {
+                    _stringCalculator = new StringCalculator();
+                    _expectedResult = 6;
+                };
+
+        Because of =
+            () => _result = _stringCalculator.Calculate(_parameter);
+
+        It should_return_6 =
+            () => _result.ShouldEqual(_expectedResult);
+
+        static StringCalculator _stringCalculator;
+        static string _parameter = "2+4";
+        static int _result;
+        static int _expectedResult;
+    }
+}
---
+++ stringCalculator/StringCalculator.cs
@@ -0,0 +1,10 @@
+namespace stringCalculator
+{
+    public class StringCalculator
+    {
+        public int Calculate(string parameter)
+        {
+            return 0;
+        }
+    }
+}
